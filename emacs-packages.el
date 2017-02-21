242ae968de7946d102972350260156d5f2cb8d0f
Brad Collins <brad@chenla.la>
2017-02-21 17:06:21 +0700

    re generated .el

--- emacs-packages.el
+++ emacs-packages.el
@@ -412,25 +412,3 @@
 ; I'm using for the videos
 (setq beacon-color "#666600")
 )
-
-;; Calfw ===================================================
-
-(use-package calfw
-  :ensure t 
-  :config
-   (require 'calfw-org)
-   (setq cfw:org-overwrite-default-keybinding t) )
-   (setq cfw:org-agenda-schedule-args '(:timestamp))
-
-   (setq calendar-day-name-array
-      ["日" "月" "火" "水" "木" "金" "土"])
-
-   ;; Unicode characters
-   (setq cfw:fchar-junction ?╋
-         cfw:fchar-vertical-line ?┃
-         cfw:fchar-horizontal-line ?━
-         cfw:fchar-left-junction ?┣
-         cfw:fchar-right-junction ?┫
-         cfw:fchar-top-junction ?┯
-         cfw:fchar-top-left-corner ?┏
-         cfw:fchar-top-right-corner ?┓)
