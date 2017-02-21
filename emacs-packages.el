b756bab51fc57eddbcfae3a1338076847ff3b80e
Deerpig <brad@chenla.la>
2017-02-21 22:10:40 +0700

    regenerated .el

--- emacs-packages.el
+++ emacs-packages.el
@@ -412,3 +412,42 @@
 ; I'm using for the videos
 (setq beacon-color "#666600")
 )
+
+;; Calfw ===================================================
+
+(use-package calfw
+  :ensure t 
+  :config
+   (require 'calfw-org)
+   (setq cfw:org-overwrite-default-keybinding t) )
+   (setq cfw:org-agenda-schedule-args '(:timestamp))
+
+   (setq calendar-day-name-array
+      ["日" "月" "火" "水" "木" "金" "土"])
+
+   ;; Unicode characters
+   (setq cfw:fchar-junction ?╋
+         cfw:fchar-vertical-line ?┃
+         cfw:fchar-horizontal-line ?━
+         cfw:fchar-left-junction ?┣
+         cfw:fchar-right-junction ?┫
+         cfw:fchar-top-junction ?┯
+         cfw:fchar-top-left-corner ?┏
+         cfw:fchar-top-right-corner ?┓)
+
+(custom-set-faces
+ '(cfw:face-title ((t (:foreground "darkgoldenrod3" :weight bold :height 2.0 :inherit variable-pitch))))
+ '(cfw:face-header ((t (:foreground "maroon2" :weight bold))))
+ '(cfw:face-sunday ((t :foreground "red" :weight bold)))
+ '(cfw:face-saturday ((t :foreground "blue" :weight bold)))
+ '(cfw:face-holiday ((t :background "grey10" :foreground "purple" :weight bold)))
+ '(cfw:face-default-content ((t :foreground "green2" )))
+ '(cfw:face-regions ((t :foreground "cyan")))
+ '(cfw:face-day-title ((t :background "grey10")))
+ '(cfw:face-today-title ((t :background "red4" :weight bold)))
+ '(cfw:face-today ((t :foreground: "cyan" :weight bold)))
+ '(cfw:face-select ((t :background "blue4")))
+;;  '(cfw:face-grid ((t :foreground "DarkGrey")))
+;;  '(cfw:face-default-day ((t :weight bold :inherit cfw:face-day-title)))
+;;  '(cfw:face-annotation ((t :foreground "RosyBrown" :inherit cfw:face-day-title)))
+ )
