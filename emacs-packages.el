df4fefa99a056b5e81213e225bfbcb55d7bd883b
Deerpig <brad@chenla.la>
2017-09-04 11:08:34 +0700

    tangled

--- emacs-packages.el
+++ emacs-packages.el
@@ -637,6 +637,7 @@
       "  25/Pomodoro: Work on helm-chronos + 5/Pomodoro: Rest"))
 )
 
+;;; Eyebrowse window manager -------------------------------
 (use-package eyebrowse
   :ensure t
   :config
@@ -646,3 +647,41 @@
   ;;(global-unset-key (kbd "C-c C-w"))
 
   (eyebrowse-mode t))
+
+;;; Rainbow Delimeters ------------------------------------
+
+(use-package rainbow-delimiters
+ :ensure t
+ :config )
+
+;;; AucTex -------------------------------------------------
+
+(use-package tex-site
+  :ensure auctex
+  :mode ("\\.tex\\'" . latex-mode)
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
+  (add-hook 'LaTeX-mode-hook
+            (lambda ()
+              (rainbow-delimiters-mode)
+              (smartparens-mode)
+              (turn-on-reftex)
+              (setq reftex-plug-into-AUCTeX t)
+              (reftex-isearch-minor-mode)
+              (setq TeX-PDF-mode t)
+              (setq TeX-source-correlate-method 'synctex)
+              (setq TeX-source-correlate-start-server t)))
+
+;; Update PDF buffers after successful LaTeX runs
+(add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
+           #'TeX-revert-document-buffer)
+
+;; to use pdfview with auctex
+(add-hook 'LaTeX-mode-hook 'pdf-tools-install)
+
+;; to use pdfview with auctex
+(setq TeX-view-program-selection '((output-pdf "pdf-tools"))
+       TeX-source-correlate-start-server t)
+(setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view"))))
